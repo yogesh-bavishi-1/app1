74b0743681da3f1b5a4430cecb81fbf01135395f
yogesh-bavishi-1 <yogesh@improwised.com>
2021-01-12 17:55:05 +0530

    initiall

---
+++ database/factories/AddStudentFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\AddStudent;
+use Faker\Generator as Faker;
+
+$factory->define(AddStudent::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
